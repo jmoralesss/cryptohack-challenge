56b7f1116277c77b769cc69b2fd176e2de71bc97
jmoralesss <mail@jmorales.net>
2021-08-25 22:16:44 -0500

    Updated solution to hex

--- encoding/hex/Main.hs
+++ encoding/hex/Main.hs
@@ -8,7 +8,7 @@
     63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d
 -}
 
-import Text.XML.HXT.DOM.Util
+import Text.XML.HXT.DOM.Util (hexStringToInt)
 import Data.List.Split (chunksOf)
 import Data.Char (chr)
 
